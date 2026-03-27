737da0e704d32eb3bd3c794ff6651f822a4c964b
PANicAndCode <totallynotascamboi@gmail.com>
2026-03-26 22:04:20 -0500

    Add files via upload

--- supabase-config.js
+++ supabase-config.js
@@ -1,3 +1,4 @@
 window.SUPABASE_CONFIG = {
-  url: "https://eyzrvijtlfhnsnsvxtaj.supabase.co",
-  anonKey: "JhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV5enJ2aWp0bGZobnNuc3Z4dGFqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ1NzY0MTIsImV4cCI6MjA5MDE1MjQxMn0.WDLt-0nTyDY0ILr54zKvagaIDJo-WRVKwHL4RLrDy-s
+  url: "PASTE_YOUR_SUPABASE_PROJECT_URL",
+  anonKey: "PASTE_YOUR_SUPABASE_ANON_KEY"
+};
